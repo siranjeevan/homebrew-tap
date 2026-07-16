7df9c09d97538a2e08accb692ad5735e18bb353d
Siranjeevan <smartjeevith2k@gmail.com>
2026-07-16 02:46:55 -0700

    Update Clipt to v1.0.4

--- Casks/clipt.rb
+++ Casks/clipt.rb
@@ -1,6 +1,6 @@
 cask "clipt" do
-  version "1.0.3"
-  sha256 "1765ee1ad438ef665dc8d8d64b2574ab0d44cf1e8cd30984af2c59d45d49a6ed"
+  version "1.0.4"
+  sha256 "383a13433239ecee4b4ca6f42c1cb7eb5bea76e82e385e092ae5e6b4a1f15456"
 
   url "https://github.com/siranjeevan/Clipt/releases/download/v#{version}/Clipt.zip"
   name "Clipt"
