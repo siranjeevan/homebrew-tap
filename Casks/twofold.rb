17586c4a08b7e349e5594fa4b6b80c6947552abb
Siranjeevan <siranjeevan20@gmail.com>
2026-07-20 17:28:29 +0530

    Update sha256 checksum for Twofold cask

--- Casks/twofold.rb
+++ Casks/twofold.rb
@@ -1,6 +1,6 @@
 cask "twofold" do
   version "1.0.0"
-  sha256 "73e5685251bec8153b6dba03f4e4f8b9f5ed3140ae3000d1224f28e3a9e4b112"
+  sha256 "223cc2335d27e63e511365a0bca3f7293a68218da97c802111d9c94f20e5bba4"
 
   url "https://github.com/siranjeevan/Twofold/releases/download/v#{version}/Twofold.zip"
   name "Twofold"
